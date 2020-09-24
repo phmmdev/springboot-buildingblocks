9c638d60928be6e442984a3472c2e7b237e3d751
phmmdev <phmmdev@gmail.com>
2020-09-24 01:00:42 -0300

    adding and configuring h2 databse, few test to learn how it works

---
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -0,0 +1,109 @@
+package com.stacksimplify.restservices.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "FIRST_NAME", length = 50, nullable = false)
+    private  String firstname;
+
+    @Column(name = "LAST_NAME", length = 50, nullable = false)
+    private  String lastname;
+
+    @Column(name = "EMAIL_ADDRESS", length = 50, nullable = false)
+    private  String email;
+
+    @Column(name = "ROLE", length = 50, nullable = false)
+    private  String role;
+
+    @Column(name = "SSN", length = 50, nullable = false, unique = true)
+    private  String ssn;
+
+    public User(Long id, String username, String firstname, String lastname, String email, String role, String ssn) {
+        this.id = id;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.role = role;
+        this.ssn = ssn;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", ssn='" + ssn + '\'' +
+                '}';
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values(101, 'pedro101@gmail.com', 'Pedro', 'Moraes', 'admin', 'ssn101', 'phmmdev101')
+insert into user values(102, 'pedro102@gmail.com', 'Pedro', 'Moraes', 'admin', 'ssn102', 'phmmdev102')
+insert into user values(103, 'pedro103@gmail.com', 'Pedro', 'Moraes', 'admin', 'ssn103', 'phmmdev103')
+
